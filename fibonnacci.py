c33212b379e1fbedc69f662fb3c6c1d20d403e3e
Felipe Braiani <felipe.braianitech@gmail.com>
2024-10-10 11:12:44 -0400

    Atividade de Fibonnacci

--- fibonnacci.py
+++ fibonnacci.py
@@ -1,3 +1,22 @@
 #
 # - Criar função para exibir os n primeiros números da sequência fibbonacci
 #
+def fibonnacci(steps):
+    sequencia = [0,1]
+    if steps <= 2:
+        return sequencia
+    
+    for i in range(2, steps):
+        soma = sequencia[i-1] + sequencia[i-2]
+        sequencia.append(soma)
+    
+    return sequencia
+    
+num = int(input("Digite quantos números da sequência de Fibonnacci deseja ver: "))
+
+if num < 3:
+    print("O mínimo de elementos são 2!")
+
+sequencia = fibonnacci(num)
+
+print(f"Sequência de Fibo: {sequencia}")
