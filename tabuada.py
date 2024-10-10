6bd707e2246a88e86b589d4959bec5d2e4ebc8f0
Felipe Braiani <felipe.braianitech@gmail.com>
2024-10-10 11:04:16 -0400

    Atividade da  tabuada finalizada

--- tabuada.py
+++ tabuada.py
@@ -1,3 +1,14 @@
 #
 # - Criar tabuada com o número informado pelo usuário
 #
+
+tabuada = int(input("Digite qual a tabuada você deseja: "))
+multiplicador = int(input("Você deseja o cálculo até qual valor: "))
+
+
+if tabuada < 0 or multiplicador < 1:
+    print("Valores inválidos!")
+    exit()
+
+for i in range(multiplicador + 1):
+    print(f"{tabuada} x {i} = {tabuada * i}")
