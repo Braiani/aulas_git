dce9e4ab71d11351abdad8b9715eb804ad3cda1b
Felipe Braiani <felipe.braianitech@gmail.com>
2024-10-10 11:13:24 -0400

    Ajuste na atividade de fatorial

--- fatorial.py
+++ fatorial.py
@@ -6,7 +6,7 @@ num = int(input("Digite o número que deseja calcular o fatorial (!): "))
 
 resultado = 1
 
-if num >= 1:
+if num > 1:
     resultado = num
     for i in range((num - 1), 0, -1):
         resultado *= i
