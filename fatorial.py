6ee5961c5242bfdf2901ebd5dbd9aa2a0ce54f36
Felipe Braiani <felipe.braianitech@gmail.com>
2024-10-10 10:50:59 -0400

    Atualizado código do fatorial

--- fatorial.py
+++ fatorial.py
@@ -1,7 +1,14 @@
+#
+# - Criar função para calcular fatorial de valor informado pelo usuario
+#
+
 num = int(input("Digite o número que deseja calcular o fatorial (!): "))
 
-resultado = num
-for i in range((num - 1), 0, -1):
-    resultado *= i
+resultado = 1
+
+if num >= 1:
+    resultado = num
+    for i in range((num - 1), 0, -1):
+        resultado *= i
 
 print(f"Resultado do fatorial: {resultado}")
