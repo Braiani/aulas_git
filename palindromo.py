3ee3ddac4d10e27ef12da4fc153f77cab549a9b5
Felipe Braiani <felipe.braianitech@gmail.com>
2024-10-15 07:44:28 -0400

    Atividade de Palindromo finalizada

--- palindromo.py
+++ palindromo.py
@@ -1,3 +1,10 @@
 #
 # - Criar função para verificar se a palavra ou frase é palindromo
 #
+
+palavra = input("Digite a palavra que deseja verificar se é ou não Palindromo: ")
+
+if palavra == ''.join(reversed(palavra)):
+    print("A palavra digitada É palindromo!")
+else:
+    print("A palavra digitada NÃO É palindromo!")
