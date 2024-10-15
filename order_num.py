f3c382f6d80a4c2995da3afb5bd6166b62034782
Felipe Braiani <felipe.braianitech@gmail.com>
2024-10-15 07:42:23 -0400

    Atividade de ordenação de número de forma crescente finalizada

--- order_num.py
+++ order_num.py
@@ -1,3 +1,25 @@
 #
 # - Criar função para ordenar uma lista de números de forma crescente
 #
+
+lista = []
+while True:
+    try:
+        lista.append(int(input("Digite um número para adicionar na lista (ctrl + c para ordenar): ")))
+    except KeyboardInterrupt:
+        break
+
+print(f"Lista digitada por você: {lista}")
+
+swapped = True
+while swapped:
+    swapped = False
+    for i in range(len(lista) - 1):
+        temp = None
+        if lista[i] > lista[i + 1]:
+                temp = lista[i]
+                lista[i] = lista[i + 1]
+                lista[i + 1] = temp
+                swapped = True
+
+print(f"Lista ordenada: {lista}")
