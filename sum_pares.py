b628828ac09cfd7db8304377e565108f89ddb961
Felipe Braiani <felipe.braianitech@gmail.com>
2024-10-15 08:01:08 -0400

    Atividade de soma de números pares finalizada

--- sum_pares.py
+++ sum_pares.py
@@ -1,3 +1,14 @@
 #
 # - Receber 10 números e somar os pares
 #
+
+lista = []
+soma = 0
+for i in range(10):
+    digitado = int(input(f"Digite o {i+1}º número inteiro na lista: "))
+    lista.append(digitado)
+    if digitado % 2 == 0:
+        soma += digitado
+
+print(f"Os números digitados foram: {lista}")
+print(f"A soma dos números digitados foi: {soma}")
